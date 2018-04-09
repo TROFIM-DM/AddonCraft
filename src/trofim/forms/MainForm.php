d0cadf903b7b21c23c7b2d34bbf8a3cf4e94656b
Dmitry Trofimov <Minecraft.trofim@gmail.com>
2018-04-10 00:19:28 +0300

    Minor design bugs fixed

--- src/trofim/forms/MainForm.php
+++ src/trofim/forms/MainForm.php
@@ -1,13 +1,16 @@
 <?php
 namespace trofim\forms;
 
-use Exception;
-use facade\Json;
-use php\compress\ZipFile;
-use windows;
 use std, gui, framework, trofim;
+use Exception;
+use php\compress\ZipFile;
 
-
+/**
+ * Класс формы MainForm.
+ * 
+ * @author TROFIM
+ * @url https://github.com/TROFIM-YT/AddonCraft
+ */
 class MainForm extends AbstractForm
 {
     
@@ -16,13 +19,14 @@ class MainForm extends AbstractForm
      * 
      * @event construct 
      */
-    function doConstruct(UXEvent $e = null)
+    function doConstruct (UXEvent $e = null)
     {
         $this->on('dragOver', function (UXDragEvent $drag) {
             if (count($drag->dragboard->files) == 1) {
                 if (($this->tabPane->selectedIndex == 0 && fs::ext($drag->dragboard->files[0]->getName()) == 'jar') ||
                     ($this->tabPane->selectedIndex == 1 && fs::ext($drag->dragboard->files[0]->getName()) == 'zip') ||
-                    ($this->tabPane->selectedIndex == 2 && fs::ext($drag->dragboard->files[0]->getName()) == 'zip')) {
+                    ($this->tabPane->selectedIndex == 2 && fs::ext($drag->dragboard->files[0]->getName()) == 'zip') ||
+                    ($this->tabPane->selectedIndex == 3 && fs::isDir($drag->dragboard->files[0]->getPath()))) {
                     $drag->acceptTransferModes(['MOVE', 'COPY']);
                 }
             }
@@ -32,16 +36,19 @@ class MainForm extends AbstractForm
             if (count($drag->dragboard->files)) {
                 foreach ($drag->dragboard->files as $file) {
                     if ($this->tabPane->selectedIndex == 0 && fs::ext($file->getName()) == 'jar') {
-                        ApiMods::addMod($file);
+                        ApiMods::add($file);
                     } else if ($this->tabPane->selectedIndex == 1 && fs::ext($file->getName()) == 'zip') {
-                        ApiTextures::addTexture($file);
+                        ApiTextures::add($file);
                     } else if ($this->tabPane->selectedIndex == 2 && fs::ext($file->getName()) == 'zip') {
-                        ApiShaders::addShader($file);
+                        ApiShaders::add($file);
+                    } else if ($this->tabPane->selectedIndex == 3 && fs::isDir($file->getPath())) {
+                        ApiMaps::add($file);
                     }
                 }
                 return;
             }
         });
+        Wiki::switch(0);
     }
     
     /**
@@ -49,21 +56,19 @@ class MainForm extends AbstractForm
      * 
      * @event show 
      */
-    function doShow(UXWindowEvent $e = null)
+    function doShow (UXWindowEvent $e = null)
     {    
-        /*Плавное появление окна*/
+        // Плавное появление окна
         Animation::fadeOut($this, 1, function () {
             $this->panelInfoMod->visible = false;
             Animation::fadeIn($this, 350);
         });
-        
-        
     }
     
     /**
      * @event imageHide.click-Left 
      */
-    function doImageHideClickLeft(UXMouseEvent $e = null)
+    function doImageHideClickLeft (UXMouseEvent $e = null)
     {    
         $this->iconified = !$this->iconified;
     }
@@ -71,39 +76,55 @@ class MainForm extends AbstractForm
     /**
      * @event imageExit.click-Left 
      */
-    function doImageExitClickLeft(UXMouseEvent $e = null)
+    function doImageExitClickLeft (UXMouseEvent $e = null)
     {    
         Animation::fadeOut($this, 350, function () {
             app()->shutdown();
             exit();
         });
     }
-
+    
+    /**
+     * @event buttonWiki.action 
+     */
+    function doButtonWikiAction (UXEvent $e = null)
+    {    
+        Wiki::open();
+    }
+    
+    /**
+     * @event tabPane.change 
+     */
+    function doTabPaneChange (UXEvent $e = null)
+    {    
+        Wiki::switch($e->sender->selectedIndex);
+    }
+    
     /**
      * @event boxMods.action 
      */
-    function doBoxModsAction(UXEvent $e = null)
+    function doBoxModsAction (UXEvent $e = null)
     {
         if ($e->sender->items->count > 0 && $e->sender->selectedIndex > -1) {
             if (!$this->panelInfoMod->visible) $this->panelInfoMod->visible = true;
             $this->boxInfoMod->items->clear();
-            DesignMods::showInfoMod(AddonCraft::$listMods[$e->sender->selectedIndex]);
+            DesignMods::showInfo(ApiMods::getObjects()[$e->sender->selectedIndex]);
         }
     }
 
     /**
      * @event buttonDeleteMod.action 
      */
-    function doButtonDeleteModAction(UXEvent $e = null)
+    function doButtonDeleteModAction (UXEvent $e = null)
     {    
         $alert = new UXAlert('INFORMATION');
-        $alert->title = 'AddonCraft';
-        $alert->headerText = 'Удаление мода...';
-        $alert->contentText = 'Вы действительно хотите удалить мод?';
-        $alert->setButtonTypes(['Да', 'Нет']);
-        $alert->graphic = new UXImageView(new UXImage('res://.data/img/delete_alert.png'));
+        $alert->title = app()->getName();
+        $alert->headerText = Language::translate('mainform.message.mods.delete.header');
+        $alert->contentText = Language::translate('mainform.message.mods.delete.content');
+        $alert->setButtonTypes([Language::translate('word.yes'), Language::translate('word.no')]);
+        $alert->graphic = new UXImageView(new UXImage('res://.data/img/icon/delete_alert-24.png'));
         
-        $textUrl = new UXLabelEx(AddonCraft::$listMods[$this->boxMods->selectedIndex]['name']);
+        $textUrl = new UXLabelEx(ApiMods::getObjects()[$this->boxMods->selectedIndex]['name']);
         $textUrl->style = '-fx-font-family: "Impact"; -fx-font-size: 22px; -fx-text-alignment: CENTER; -fx-alignment: CENTER;';
         $box = new UXVBox([$textUrl]);
         $box->style = '-fx-alignment: CENTER;';
@@ -112,8 +133,8 @@ class MainForm extends AbstractForm
         $alert->expanded = true;
         
         switch ($alert->showAndWait()) {
-            case 'Да':
-                ApiMods::deleteMod($this->boxMods->selectedIndex);
+            case Language::translate('word.yes'):
+                ApiMods::delete($this->boxMods->selectedIndex);
             break;
         }
     }
@@ -121,123 +142,238 @@ class MainForm extends AbstractForm
     /**
      * @event buttonOpenMod.action 
      */
-    function doButtonOpenModAction(UXEvent $e = null)
+    function doButtonOpenModAction (UXEvent $e = null)
     {    
-        execute('explorer.exe  /select, ' . AddonCraft::$listMods[$this->boxMods->selectedIndex]['path']['mod']);
+        execute('explorer.exe  /select, ' . ApiMods::getObjects()[$this->boxMods->selectedIndex]['path']['mod']);
     }
 
     /**
      * @event buttonUpdateMod.action 
      */
-    function doButtonUpdateModAction(UXEvent $e = null)
+    function doButtonUpdateModAction (UXEvent $e = null)
     {    
-        $this->buttonUpdateMod->enabled = false;
+        $e->sender->enabled = false;
         
-        AddonCraft::clearValue('listMods');
+        ApiMods::clearValue('objectsInfo');
         $this->panelInfoMod->visible = false;
         $this->boxMods->items->clear();
         $this->boxInfoMod->items->clear();
         
-        ApiMods::findMods();
+        ApiMods::find();
         
-        waitAsync(3000, function () {
-            $this->buttonUpdateMod->enabled = true;
+        waitAsync(3000, function () use ($e) {
+            $e->sender->enabled = true;
         });
     }
 
     /**
      * @event buttonModeMod.action 
      */
-    function doButtonModeModAction(UXEvent $e = null)
+    function doButtonModeModAction (UXEvent $e = null)
     {
         if ($this->boxMods->selectedIndex > -1) 
             ApiMods::setMode($this->boxMods->selectedIndex);
     }
 
     /**
-     * @event boxEnTextures.action 
+     * @event boxEnTextures.action
      */
-    function doBoxEnTexturesAction(UXEvent $e = null)
+    function doBoxEnTexturesAction (UXEvent $e = null)
     {    
-        if ($e->sender->selectedIndex > -1) $this->boxTextures->selectedIndex = -1;
+        if ($e->sender->selectedIndex > -1)
+            $this->boxTextures->selectedIndex = -1;
     }
 
     /**
      * @event boxTextures.action 
      */
-    function doBoxTexturesAction(UXEvent $e = null)
+    function doBoxTexturesAction (UXEvent $e = null)
     {    
-        if ($e->sender->selectedIndex > -1) $this->boxEnTextures->selectedIndex = -1;
+        if ($e->sender->selectedIndex > -1)
+            $this->boxEnTextures->selectedIndex = -1;
     }
 
     /**
      * @event buttonUpdateTexture.action 
      */
-    function doButtonUpdateTextureAction(UXEvent $e = null)
+    function doButtonUpdateTextureAction (UXEvent $e = null)
     {    
-        $this->buttonUpdateTexture->enabled = false;
+        $e->sender->enabled = false;
         
-        AddonCraft::clearValue('listTextures');
+        ApiTextures::clearValue('objectsInfo');
         $this->boxTextures->items->clear();
         $this->boxEnTextures->items->clear();
         
-        ApiTextures::findTextures();
+        ApiTextures::find();
         
-        waitAsync(3000, function () {
-            $this->buttonUpdateTexture->enabled = true;
+        waitAsync(3000, function () use ($e) {
+            $e->sender->enabled = true;
         });
     }
 
     /**
      * @event buttonFolderTexture.action 
      */
-    function doButtonFolderTextureAction(UXEvent $e = null)
+    function doButtonFolderTextureAction (UXEvent $e = null)
     {    
-        open(AddonCraft::getPathMinecraft() . '\\resourcepacks\\');
+        open(Path::getPathMinecraft() . '\\resourcepacks\\');
     }
 
     /**
      * @event boxShaders.action 
      */
-    function doBoxShadersAction(UXEvent $e = null)
+    function doBoxShadersAction (UXEvent $e = null)
     {    
         if ($e->sender->items->count() > 0)
-            ApiShaders::selectedShader($e->sender->selectedIndex);
+            ApiShaders::selected($e->sender->selectedIndex);
     }
 
     /**
      * @event buttonFolderShader.action 
      */
-    function doButtonFolderShaderAction(UXEvent $e = null)
+    function doButtonFolderShaderAction (UXEvent $e = null)
     {    
-        open(AddonCraft::getPathMinecraft() . '\\shaderpacks\\');
+        open(Path::getPathMinecraft() . '\\shaderpacks\\');
     }
 
     /**
      * @event buttonUpdateShader.action 
      */
-    function doButtonUpdateShaderAction(UXEvent $e = null)
+    function doButtonUpdateShaderAction (UXEvent $e = null)
     {    
-        $this->buttonUpdateShader->enabled = false;
+        $e->sender->enabled = false;
         
-        AddonCraft::clearValue('listShaders');
-        AddonCraft::$listShaders = ['list' => [['name' => 'OFF'], ['name' => '(internal)']]];
+        ApiShaders::clearValue('objectsInfo');
         $this->boxShaders->items->clear();
-        $this->boxShaders->items->addAll(['Нет', '(Встроенный)']);
+        $this->boxShaders->items->addAll([Language::translate('word.no'), '(' . Language::translate('word.internal') . ')']);
         
-        ApiShaders::findShaders();
+        ApiShaders::find();
         
-        waitAsync(3000, function () {
-            $this->buttonUpdateShader->enabled = true;
+        waitAsync(3000, function () use ($e) {
+            $e->sender->enabled = true;
         });
     }
 
-    function setModeMod ($mode) {
+    /**
+     * @event boxShaders.construct 
+     */
+    function doBoxShadersConstruct (UXEvent $e = null)
+    {    
+        $e->sender->items->addAll([Language::translate('word.no'), '(' . Language::translate('word.internal') . ')']);
+    }
+
+    /**
+     * @event boxMaps.action 
+     */
+    function doBoxMapsAction (UXEvent $e = null)
+    {    
+        if ($e->sender->selectedIndex != -1 && !$this->buttonEditMap->enabled)
+            $this->buttonDeleteMap->enabled =
+            $this->buttonFolderMap->enabled =
+            $this->buttonEditMap->enabled = true;
+    }
+
+    /**
+     * @event buttonFolderMap.action 
+     */
+    function doButtonFolderMapAction (UXEvent $e = null)
+    {    
+        open(ApiMaps::getObjects()[$this->boxMaps->selectedIndex]['path']['map']);
+    }
+
+    /**
+     * @event buttonUpdateMap.action 
+     */
+    function doButtonUpdateMapAction (UXEvent $e = null)
+    {    
+        $e->sender->enabled = false;
+        
+        ApiMaps::clearValue('objectsInfo');
+        $this->boxMaps->items->clear();
+        
+        ApiMaps::find();
+        
+        if ($this->buttonEditMap->enabled)
+            $this->buttonDeleteMap->enabled =
+            $this->buttonFolderMap->enabled =
+            $this->buttonEditMap->enabled = false;
+        
+        waitAsync(3000, function () use ($e) {
+            $e->sender->enabled = true;
+        });
+    }
+
+    /**
+     * @event buttonDeleteMap.action 
+     */
+    function doButtonDeleteMapAction (UXEvent $e = null)
+    {    
+        $alert = new UXAlert('INFORMATION');
+        $alert->title = app()->getName();
+        $alert->headerText = Language::translate('mainform.message.maps.delete.header');
+        $alert->contentText = Language::translate('mainform.message.maps.delete.content');
+        $alert->setButtonTypes([Language::translate('word.yes'), Language::translate('word.no')]);
+        $alert->graphic = new UXImageView(new UXImage('res://.data/img/icon/delete_alert-24.png'));
+        
+        $textUrl = new UXLabelEx(ApiMaps::getObjects()[$this->boxMaps->selectedIndex]['info']['LevelName']);
+        $textUrl->style = '-fx-font-family: "Impact"; -fx-font-size: 22px; -fx-text-alignment: CENTER; -fx-alignment: CENTER;';
+        $box = new UXVBox([$textUrl]);
+        $box->style = '-fx-alignment: CENTER;';
+        
+        $alert->expandableContent = $box;
+        $alert->expanded = true;
+        
+        switch ($alert->showAndWait()) {
+            case Language::translate('word.yes'):
+                ApiMaps::delete($this->boxMaps->selectedIndex);
+            break;
+        }
+    }
+
+    /**
+     * @event buttonEditMap.action 
+     */
+    function doButtonEditMapAction (UXEvent $e = null)
+    {    
+        app()->form(EditMapForm)->showForm($this->boxMaps->selectedIndex);
+    }
+
+    /**
+     * @event buttonUpdateVersion.action 
+     */
+    function doButtonUpdateVersionAction (UXEvent $e = null)
+    {    
+        $e->sender->enabled = false;
+        
+        AddonCraft::clearValue('listVersions');
+        $this->boxVersions->items->clear();
+        
+        ApiVersions::findVersions();
+        
+        waitAsync(3000, function () use ($e) {
+            $e->sender->enabled = true;
+        });
+    }
+    
+    /**
+     * @event buttonAlt.action 
+     */
+    function doButtonAltAction (UXEvent $e = null)
+    {    
+        $parse = new Parsedown();
+        $TEXT = $parse->text("# Как установить мод в AddonCraft?\n1. Нажать кнопку \"**Добавить мод...**\" или перенести мод в окно программы.\n2. Дождаться установки...\n3. PROFIT!");
+        pre($TEXT);
+        $progress = new Progress($this->boxMods, 'Всем привет, Дорогие друзья!' . "\n" . 'Выскажите свое слово!');
+        waitAsync(4000, function () use ($progress){$progress->free();});
+    }
+
+    function setModeMod ($mode)
+    {
         if ($mode) {
-            $this->buttonModeMod->text = 'Отключить';
+            $this->buttonModeMod->text = Language::translate('mainform.mods.btn.disabled');
             $this->buttonModeMod->textColor = '#b31a1a';
         } else {
-            $this->buttonModeMod->text = 'Включить';
+            $this->buttonModeMod->text = Language::translate('mainform.mods.btn.enabled');
             $this->buttonModeMod->textColor = '#00e209';
         }
     }
