a5e3b6d3b08d3b546788d224aa7ac78e65ac45ed
Dmitry Trofimov <Minecraft.trofim@gmail.com>
2018-04-10 00:17:14 +0300

    Add function and fixed bugs

--- src/trofim/scripts/system/Dir.php
+++ src/trofim/scripts/system/Dir.php
@@ -1,23 +1,23 @@
 <?php
 namespace trofim\scripts\system;
 
-use trofim;
-use std;
+use trofim, std;
 
 /**
- * Класс Dir для работы с папками и содержимым.
+ * Класс для работы с папками и содержимым.
  * 
  * @author TROFIM
+ * @url https://github.com/TROFIM-YT/AddonCraft
  */
 class Dir {
 
     /**
      * Удалить папку и файлы, лежащие в папке.
      * 
-     * @param $pathDir Путь к папке.
-     * @param bool
+     * @param string $pathDir Путь к папке.
+     * @return bool
      */
-    public static function delete ($pathDir) : bool
+    public static function delete (string $pathDir) : bool
     {
         if ($dir = new File($pathDir)->findFiles()) {
             foreach ($dir as $path)
@@ -28,7 +28,6 @@ class Dir {
                     (fs::isDir($file)) ? self::delete($file): fs::delete($file);
             }
         }
-        
         return fs::delete($pathDir);
     }
     
@@ -36,11 +35,11 @@ class Dir {
      * Копировать и переместить папку и файлы, находящиеся в папке.
      * (Папка должна существовать).
      * 
-     * @param $fromDir Путь к папке из которой.
-     * @param $toDir Путь к папке в которую.
+     * @param string $fromDir Путь к папке из которой.
+     * @param string $toDir Путь к папке в которую.
      * @return bool
      */
-    public static function copy ($fromDir, $toDir) : bool
+    public static function copy (string $fromDir, string $toDir) : bool
     {
         if (fs::exists($fromDir = fs::abs($fromDir)) && fs::exists($toDir = fs::abs($toDir))) {
             if ($dir = new File($fromDir)->findFiles()) {
@@ -59,8 +58,30 @@ class Dir {
                 }
             }
         }
-        
         return false;
+    }
+    
+    /**
+     * Переместить папку и файлы, находящиеся в папке.
+     * 
+     * @param string $fromDir Путь к папке откуда.
+     * @param string $toDir Путь к папке куда.
+     * @return bool
+     */
+    public static function move (string $fromDir, string $toDir) : bool
+    {
+        return fs::move($fromDir, $toDir);
+    }
+    
+    /**
+     * Проверить папка ли это.
+     * 
+     * @param string $pathDir Путь к папке.
+     * @return bool
+     */
+    public static function isDir (string $pathDir) : bool
+    {
+        return fs::isDir($pathDir);
     }
     
 }
